9ba91bbbec98c77f3a4005cfc42c50693dcca198
AdrianIgl <adrian.igldel@educa.jcyl.es>
2021-02-23 18:53:15 +0100

    final

--- src/Components/CRUD.js
+++ src/Components/CRUD.js
@@ -1,149 +1,155 @@
 
 //npm i bootstrap reactstrap axios sweetalert
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import axios from 'axios';
 //libreria para mejorar los alert   https://sweetalert.js.org/guides/
 //npm install sweetalert --save
 import swal from 'sweetalert';
 function CRUD() {
-    //direccion de la API
-    const baseUrl="http://localhost:4004/users/";
-    const [data, setData]=useState([]);
-    const [modalInsertar, setModalInsertar]= useState(false);
-    const [modalEditar, setModalEditar]= useState(false);
-    const [modalEliminar, setModalEliminar]= useState(false);
-    const [frameworkSeleccionado, setFrameworkSeleccionado]=useState({
-      id: '',
-      firstName: '',
-      lastName: ''
-      
-    });
-  
-    const handleChange=e=>{
-      const {name, value}=e.target;
-      setFrameworkSeleccionado((prevState)=>({
-        ...prevState,
-        [name]: value
-      }))
-      console.log(frameworkSeleccionado);
-    }
-  
-    const abrirCerrarModalInsertar=()=>{
-      setModalInsertar(!modalInsertar);
-    }
-  
-    const abrirCerrarModalEditar=()=>{
-      setModalEditar(!modalEditar);
-    }
-  
-    const abrirCerrarModalEliminar=()=>{
-      setModalEliminar(!modalEliminar);
-    }
-  
-    const peticionGet=async()=>{
-      
-     
-      await axios.get(baseUrl)
-      .then(response=>{
+  //direccion de la API
+  const baseUrl = "http://localhost:4004/proyectofinal/";
+  const [data, setData] = useState([]);
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
+  const [modalEliminar, setModalEliminar] = useState(false);
+  const [frameworkSeleccionado, setFrameworkSeleccionado] = useState({
+    id: '',
+    comunidad: '',
+    pzifer: '',
+    moderna: '',
+    dosis_administradas: '',
+    pauta_completa: '',
+    
+
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFrameworkSeleccionado((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
+    console.log(frameworkSeleccionado);
+  }
+
+  const abrirCerrarModalInsertar = () => {
+    setModalInsertar(!modalInsertar);
+  }
+
+  const abrirCerrarModalEditar = () => {
+    setModalEditar(!modalEditar);
+  }
+
+  const abrirCerrarModalEliminar = () => {
+    setModalEliminar(!modalEliminar);
+  }
+
+  const peticionGet = async () => {
+
+
+    await axios.get(baseUrl)
+      .then(response => {
         setData(response.data);
         //console.log(response.data);
-      }).catch(error=>{
+      }).catch(error => {
         console.log(error);
       })
-    }//peticionGet
-  
-    const peticionPost=async()=>{
-      const usuario={
-        firstName:frameworkSeleccionado.firstName,
-        lastName:frameworkSeleccionado.lastName
-      };
-      
-      await axios.post(baseUrl+"insertar/", usuario)
-      .then(response=>{
-       
-        //cerramos la ventana modal
+  }//peticionGet
+
+  const peticionPost = async () => {
+    const vacuna = {
+      comunidad: frameworkSeleccionado.comunidad,
+      pzifer: frameworkSeleccionado.pzifer,
+      moderna: frameworkSeleccionado.moderna,
+      dosis_administradas: frameworkSeleccionado.dosis_administradas,
+      pauta_completa: frameworkSeleccionado.pauta_completa,
+   
+    };
+
+    await axios.post(baseUrl + "insertar/", vacuna)
+      .then(response => {
+
         abrirCerrarModalInsertar();
-        //refresco la tabla haciendo una peticion get
         peticionGet();
-        
-      }).catch(error=>{
+
+      }).catch(error => {
         console.log(error);
       })
-    }//peticionPost
-  
-    const peticionPut=async()=>{
-      
-      
-      const usuario={
-        firstName:frameworkSeleccionado.firstName,
-        lastName:frameworkSeleccionado.lastName
-      };
-      await axios.put(baseUrl+"modificar/"+frameworkSeleccionado.id,usuario)
-      .then(response=>{
-        if (response.data!=null)
-        {
-         //swal("Good job!", "You clicked the button!", "success"); 
-          swal("Buen trabajo!","Registro Modificado Satisfactoriamente","success");
-         
+  }//peticionPost
+
+  const peticionPut = async () => {
+
+
+    const vacuna = {
+      comunidad: frameworkSeleccionado.comunidad,
+      pzifer: frameworkSeleccionado.pzifer,
+      moderna: frameworkSeleccionado.moderna,
+      dosis_administradas: frameworkSeleccionado.dosis_administradas,
+      pauta_completa: frameworkSeleccionado.pauta_completa,
+    };
+    await axios.put(baseUrl + "modificar/" + frameworkSeleccionado.id, vacuna)
+      .then(response => {
+        if (response.data != null) { 
+          swal("Buen trabajo!", "Registro Modificado Satisfactoriamente", "success");
           abrirCerrarModalEditar();
-           //refresco la tabla haciendo una peticion delete
-           peticionGet();
-        }  
-       
-      }).catch(error=>{
+          peticionGet();
+        }
+
+      }).catch(error => {
         console.log(error);
       })
-    }//peticionPut
-  
-    const peticionDelete=async()=>{
-     
-      axios.delete(baseUrl+"borrar/"+frameworkSeleccionado.id).then(response=>{
-      if (response.data!=null)
-      {
-        swal("Buen trabajo!","Registro Borrado Satisfactoriamente","success");
+  }//peticionPut
+
+  const peticionDelete = async () => {
+
+    axios.delete(baseUrl + "borrar/" + frameworkSeleccionado.id).then(response => {
+      if (response.data != null) {
+        swal("Buen trabajo!", "Registro Borrado Satisfactoriamente", "success");
         abrirCerrarModalEliminar();
-         //refresco la tabla haciendo una peticion delete
-         peticionGet();
+        peticionGet();
       }
-      
-       
-      }).catch(error=>{
-        console.log(error);
-       
-      })
-    }//peticionDelete
-  
-    const seleccionarFramework=(framework, caso)=>{
-      setFrameworkSeleccionado(framework);
-  
-      (caso==="Editar")?
-      abrirCerrarModalEditar():
+
+
+    }).catch(error => {
+      console.log(error);
+
+    })
+  }
+
+  const seleccionarFramework = (framework, caso) => {
+    setFrameworkSeleccionado(framework);
+
+    (caso === "Editar") ?
+      abrirCerrarModalEditar() :
       abrirCerrarModalEliminar()
-    }
-  
-    useEffect(()=>{
-      peticionGet();
-    },[])
-  
-    return (
-      <div style={{textAlign: 'center'}}>
-  <br />
-        <button className="btn btn-success" onClick={()=>abrirCerrarModalInsertar()}>Insertar</button>
-        <br /><br />
+  }
+
+  useEffect(() => {
+    peticionGet();
+  }, [])
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <br />
+      <button className="btn btn-success" onClick={() => abrirCerrarModalInsertar()}>Insertar</button>
+      <br /><br />
       <table className="table table-striped">
         <thead>
           <tr>
             <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Acciones</th>
+            <th>Comunidad</th>
+            <th>Pzifer</th>
+            <th>Moderna</th>
+            <th>Dosis administradas</th>
+            <th>Pautas completas</th>
+           
           </tr>
         </thead>
         <tbody>
-        {console.log(data[0])}
-          {data.map(framework=>(
+          {console.log(data[0])}
+          {data.map(framework => (
             <tr key={framework.id}>
               {/*console.log(framework.first_name)*/}
               {/* el nombre de los campos que vienen a continuacion tienes que ser
@@ -157,81 +163,109 @@ function CRUD() {
               
               */}
               <td>{framework.id}</td>
-              <td>{framework.firstName}</td>
-              <td>{framework.lastName}</td>
-              
-            <td>
-            <button className="btn btn-primary" onClick={()=>seleccionarFramework(framework, "Editar")}>Editar</button> 
-            <button className="btn btn-danger" onClick={()=>seleccionarFramework(framework, "Eliminar")}>Eliminar</button>
-            </td>
+              <td>{framework.comunidad}</td>
+              <td>{framework.pzifer}</td>
+              <td>{framework.moderna}</td>
+              <td>{framework.dosis_administradas}</td>
+              <td>{framework.pauta_completa}</td>
+            
+
+              <td>
+                <button className="btn btn-primary" onClick={() => seleccionarFramework(framework, "Editar")}>Editar</button>
+                <button className="btn btn-danger" onClick={() => seleccionarFramework(framework, "Eliminar")}>Eliminar</button>
+              </td>
             </tr>
           ))}
-  
-  
-        </tbody> 
-  
+
+
+        </tbody>
+
       </table>
-  
-  
+
+
       <Modal isOpen={modalInsertar}>
-        <ModalHeader>Insertar Usuarios</ModalHeader>
+        <ModalHeader>Insertar Datos de Vacunacion</ModalHeader>
         <ModalBody>
           <div className="form-group">
-            <label>First Name: </label>
+            <label>Comunidad: </label>
             <br />
-            <input type="text" className="form-control" name="firstName" onChange={handleChange}/>
+            <input type="text" className="form-control" name="comunidad" onChange={handleChange} />
             <br />
-            <label>Last Name: </label>
+            <label>Pzifer: </label>
             <br />
-            <input type="text" className="form-control" name="lastName" onChange={handleChange}/>
+            <input type="text" className="form-control" name="pzifer" onChange={handleChange} />
             <br />
-            
+            <label>Moderna: </label>
+            <br />
+            <input type="text" className="form-control" name="moderna" onChange={handleChange} />
+            <br />
+            <label>Dosis Administradas: </label>
+            <br />
+            <input type="text" className="form-control" name="dosis_administradas" onChange={handleChange} />
+            <br />
+            <label>Pauta Completa: </label>
+            <br />
+            <input type="text" className="form-control" name="pauta_completa" onChange={handleChange} />
+            <br />
           </div>
         </ModalBody>
         <ModalFooter>
-          
-          <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button>{"   "}
-          <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
+
+          <button className="btn btn-primary" onClick={() => peticionPost()}>Insertar</button>{"   "}
+          <button className="btn btn-danger" onClick={() => abrirCerrarModalInsertar()}>Cancelar</button>
         </ModalFooter>
       </Modal>
-  
+
       <Modal isOpen={modalEditar}>
-        <ModalHeader>Editar Usuarios</ModalHeader>
+        <ModalHeader>Editar Datos de la Vacunación</ModalHeader>
         <ModalBody>
           <div className="form-group">
-            <label>Nombre: </label>
+            <label>Comunidad: </label>
             <br />
-            <input type="text" className="form-control" name="firstName" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.firstName}/>
+            <input type="text" className="form-control" name="comunidad" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.comunidad} />
             <br />
-            <label>Lanzamiento: </label>
+            <label>Pzifer: </label>
             <br />
-            <input type="text" className="form-control" name="lastName" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.lastName}/>
+            <input type="text" className="form-control" name="pzifer" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.pzifer} />
             <br />
-            
+            <label>Moderna: </label>
+            <br />
+            <input type="text" className="form-control" name="moderna" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.moderna} />
+            <br />
+            <label>Dosis Administradas: </label>
+            <br />
+            <input type="text" className="form-control" name="dosis_administradas" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.dosis_administradas} />
+            <br />
+            <label>Pauta Completa: </label>
+            <br />
+            <input type="text" className="form-control" name="pauta_completa" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.pauta_completa} />
+            <br />
+        
+
           </div>
         </ModalBody>
         <ModalFooter>
-          <button className="btn btn-primary" onClick={()=>peticionPut()}>Modificar</button>{"   "}
-          <button className="btn btn-danger" onClick={()=>abrirCerrarModalEditar()}>Cancelar</button>
+          <button className="btn btn-primary" onClick={() => peticionPut()}>Modificar</button>{"   "}
+          <button className="btn btn-danger" onClick={() => abrirCerrarModalEditar()}>Cancelar</button>
         </ModalFooter>
       </Modal>
-  
+
       <Modal isOpen={modalEliminar}>
-          <ModalBody>
-          ¿Estás seguro que deseas eliminar al Usuario {frameworkSeleccionado && frameworkSeleccionado.nombre}?
+        <ModalBody>
+          ¿Estás seguro que deseas eliminar la Comunidad {frameworkSeleccionado && frameworkSeleccionado.comunidad}?
           </ModalBody>
-          <ModalFooter>
-            <button className="btn btn-danger" onClick={()=>peticionDelete()}>
-              Sí
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={() => peticionDelete()}>
+            Sí
             </button>
-            <button className="btn btn-secondary" onClick={()=>abrirCerrarModalEliminar()} >
-              No
+          <button className="btn btn-secondary" onClick={() => abrirCerrarModalEliminar()} >
+            No
             </button>
-          </ModalFooter>
-        </Modal>
-  
-      </div>
-    );
-  }
-  
-  export default CRUD;
+        </ModalFooter>
+      </Modal>
+
+    </div>
+  );
+}
+
+export default CRUD;
--- src/Components/DatosGlobales.js
+++ src/Components/DatosGlobales.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './datosg.css';
 
 const Pricing = () => {
-    return(
-        <div className="App">
-            <h1>Datos Globales</h1>
+    const baseUrl = "http://localhost:4004/proyectofinal/";
+    const [data, setData] = useState([]);
+    const [entregadas, setEntregadas] = useState([0]);
+    const [administradas, setAdministradas] = useState([0]);
+    const [pauta, setPauta] = useState([0]);
+
+
+    const peticionGet = async () => {
+        await axios.get(baseUrl)
+            .then(response => {
+                var dosisTotales = 0, dosisAdministradas = 0, pautaCompleta = 0;
+
+                setData(response.data);
+                //console.log(response.data);
+
+                response.data.map(function (item, i) {
+                    dosisTotales += item.pzifer;
+                    dosisTotales += item.moderna;
+                    dosisAdministradas += item.dosis_Administradas;
+                    pautaCompleta += item.pauta_Completa;
+                });
+
+                console.log("Dosis entregadas en CC.AA: " + dosisTotales);
+                console.log("Dosis Administradas: " + dosisAdministradas);
+                console.log("Pauta Completa: " + pautaCompleta);
+
+                setEntregadas(dosisTotales);
+                setAdministradas(dosisAdministradas);
+                setPauta(pautaCompleta);
+      
+
+                //setDosisEntregadas()
+            }).catch(error => {
+                console.log(error);
+            })
+    }//peticionGet
+    useEffect(() => {
+        peticionGet();
+    }, [])
+    return (
+        <div className="App" style={{ textAlign: 'center' }}>
+            <h1 className="gradient-text">Datos Globales</h1>
+            <div className="global">
+                <div className="bg-success rounded bg-custom-1 text-white align-middle">
+                    <p>Dosis entregadas en CC.AA</p>
+                    <p className="font-weight-bold font-size-xx-large">{entregadas.toLocaleString("es-ES")}</p>
+                </div>
+                <div className="bg-success rounded bg-custom-2 text-white align-middle">
+                    <p>Dosis administradas</p>
+                    <p className="font-weight-bold font-size-xx-large" >{administradas.toLocaleString("es-ES")}</p>
+                </div>
+                <div className="bg-success rounded bg-custom-1 text-white align-middle">
+                    <p>Nº Personas con pauta completa</p>
+                    <p className="font-weight-bold font-size-xx-large">{pauta.toLocaleString("es-ES")}</p>
+                </div>
+            </div>
         </div>
     )
 }
